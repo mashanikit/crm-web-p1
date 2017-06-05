874ec55da7ade264caa49b69c30f4ccbc60303b1
MASHA <mashanikit@gmail.com>
2017-06-04 22:14:11 -0400

    Fixed

---
+++ crm2.rb
@@ -0,0 +1,24 @@
+require_relative 'contact'
+require 'sinatra'
+
+get '/' do
+  redirect to('/home')
+end
+
+get '/home' do
+  erb :index
+end
+
+get '/contacts' do
+  @contacts = Contact.all
+  erb :contacts
+end
+
+get '/about' do
+  erb :about_me
+end
+
+
+after do
+  ActiveRecord::Base.connection.close
+end
